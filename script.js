860a76fa44e57b5f3f1f80ada73ac6098e7f5077
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-25 07:00:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
-
-
+    var boldElements = document.getElementById('paragraph').querySelectorAll('strong');
+    
+	// Add a class to each <strong> element to change its color to green
+    boldElements.forEach(function(element) {
+       element.classList.add('highlighted');
+     });
 }
 
 
-function return_normal() {
-    //Write your code here
+function returnNormal() {
+          // Get all <strong> elements within the paragraph
+          var boldElements = document.getElementById('paragraph').querySelectorAll('strong');
 
-    
+        // Remove the class from each <strong> element to revert its color back to black
+         boldElements.forEach(function(element) {
+              element.classList.remove('highlighted');
+         });
 }
